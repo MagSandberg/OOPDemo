236dfa64aafb53210b7a10d611b9de0652fcd9ec
MagnusSandbergITHS <magnus.sandberg@iths.se>
2022-09-14 10:54:57 +0200

    Add project files.

---
+++ OOPDemo/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    public class Car
+    {
+        private string _make;
+        public string Make
+        {
+            get
+            {
+                return _make;
+            }
+            set
+            {
+                _make = value;
+            }
+        }
+
+        private string _color;
+
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+
+        public void PrintCarInfo()
+        {
+            Console.WriteLine($"Car: {Make} Color: {Color}" );
+        }
+
+        //public static void PrintCarInfoStatic()
+        //{
+        //    Console.WriteLine("Det här är en statisk metod.");
+        //}
+    }
+}
---
+++ OOPDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+
+namespace OOPDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car carListOne = new Car();
+            carListOne.Make = "Volkswagen";
+            carListOne.Color = "Blue";
+
+            Car carListTwo = new Car();
+            carListTwo.Make = "Dodge";
+            carListTwo.Color = "Red";
+
+            carListOne.PrintCarInfo();
+
+            ////Skapa listor
+            //var cars = new List<Car>();
+
+            //for (int i = 0; i < 21; i++)
+            //{
+            //    var car = new Car();
+            //    car.Make = $"{i}";
+            //    car.Color = $"{i}";
+            //    cars.Add(car);
+            //}
+
+            //foreach (var item in cars)
+            //{
+            //    item.PrintCarInfo();
+            //}
+        }
+    }
+}
